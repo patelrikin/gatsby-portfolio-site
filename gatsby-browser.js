65cc57d5c31771575504542b5ddcea12ef110fcd
Rikin Patel <rpatel@anexinet.com>
2019-06-28 23:47:45 -0400

    adds polyfill for Intersection observer

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,13 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+// ES5 way
+// exports.onClientEntry = () => {
+// ES6 way
+export const onClientEntry = () => {
+  // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
+  if (typeof window.IntersectionObserver === `undefined`) {
+    import(`intersection-observer`)
+    console.log(`# IntersectionObserver is polyfilled!`)
+  }
+}
